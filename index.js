d4f876ab4740dbc4b3c01af162d1e672c0a8503e
Sonu-Shettiyar <sonubadmash003@gmail.com>
2023-07-13 13:39:41 +0530

    for_deployment

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const { connection } = require("./db");
+const { UserRouter } = require("./routes/users.routes");
+const { EmployeeRouter } = require("./routes/employees.routes");
+const app = express();
+app.use(express.json());
+require("dotenv").config()
+app.use("/users", UserRouter);
+app.use("/", EmployeeRouter);
+
+
+app.listen(process.env.port, async () => {
+    try {
+        await connection
+        console.log("DB connected")
+        console.log(`Successfully runnig on Port ${process.env.port}`)
+    } catch (error) {
+        console.log(error.message)
+    }
+})
